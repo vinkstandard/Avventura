8b43c31539ef778640e7fe41e3018f2ef9f6bceb
vinkstandard <merdacain@hotmail.it>
2025-05-14 12:51:35 +0200

    Aggiungo secondo costruttore

--- src/main/java/model/Stanza.java
+++ src/main/java/model/Stanza.java
@@ -7,11 +7,20 @@ public class Stanza {
     private Map<String, Stanza> uscite;
     private List<Oggetto> oggettiPresenti;
 
+    // costruttore completo
     public Stanza(String nome, String descrizione, Map<String, Stanza> uscite, List<Oggetto> oggettiPresenti) {
         this.nome = nome;
         this.descrizione = descrizione;
         this.uscite = uscite;
         this.oggettiPresenti = oggettiPresenti;
+    }
+
+    // costruttore semplificato (per semplificare la creazione di stanze nel main)
+    public Stanza(String nome, String descrizione) {
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.uscite = new HashMap<>();
+        this.oggettiPresenti = new ArrayList<>();
     }
 
     public void aggiungiUscita(String direzioneUscita, Stanza stanza){
@@ -30,6 +39,10 @@ public class Stanza {
 
     public String getNome() {
         return nome;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
     }
 
     public String getDescrizione() {
