2e18c4a07b5d60663defce96eb8db221cf4450bf
vinkstandard <merdacain@hotmail.it>
2025-05-14 16:35:25 +0200

    Aggiunti supporti per diversi input, aggiunta voce inventario

--- src/main/java/engine/EngineGioco.java
+++ src/main/java/engine/EngineGioco.java
@@ -14,24 +14,42 @@ public class EngineGioco {
         this.giocatore = giocatore;
     }
 
+
+
     public void avvia(){
         Scanner scanner = new Scanner(System.in);
         boolean continua = true;
 
         while(continua){
-            System.out.println("\n-" + giocatore.getStanzaAttuale().getNome()); // quesot messaggio apparirà ogni volta che ci muoveremo di stanza
+            System.out.println("\n-" + giocatore.getStanzaAttuale().getNome()); // questo messaggio apparirà ogni volta che ci muoveremo di stanza
             giocatore.getStanzaAttuale().stampaDescrizioneStanza();
             giocatore.getStanzaAttuale().stampaUscite();
-            System.out.println("\nCosa vuoi fare? ");
+            System.out.println("\n>> Cosa vuoi fare? ");
             String comando = scanner.nextLine().toLowerCase();
 
-            if(comando.startsWith("vai ")){
-                String direzione = comando.substring(4);
-                vai(direzione);
+            if(comando.startsWith("vai ") || comando.equals("nord") || comando.equals("sud") || comando.equals("ovest") || comando.equals("est")){
+                String direzione = comando;
+                if(comando.charAt(0) == 'v') {
+                    direzione = comando.substring(4);
+                    vai(direzione);
+                }else{
+                    vai(direzione);
+                }
             }
-            else if(comando.startsWith("prendi ")){
-                String nomeOggetto = comando.substring(7); // rimuove dal comando prendi, "prendi ", es. "prendi pala", substring(7) == "pala"; stessa cosa con il "vai "
-                prendi(nomeOggetto);
+
+            else if(comando.startsWith("prendi ") || comando.startsWith("raccogli ") ){
+                String nomeOggetto;
+                // rimuove dal comando prendi, "prendi ", es. "prendi pala", substring(7) == "pala"; stessa cosa con il "vai "
+                switch (Character.toLowerCase(comando.charAt(0))) {
+                    case 'p':
+                        nomeOggetto = comando.substring(7);
+                        prendi(nomeOggetto);
+                        break;
+                    case 'r':
+                        nomeOggetto = comando.substring(9);
+                        prendi(nomeOggetto);
+                        break;
+                }
 
             }
             else if(comando.equals("guarda")){
@@ -39,9 +57,12 @@ public class EngineGioco {
             }
             else if(comando.startsWith("esci")){
                 continua = false;
-                System.out.println("Uscita dal gioco...");
+                System.out.println(">> Uscita dal gioco...");
+            }
+            else if(comando.equals("inventario")){
+                giocatore.getInventario().visualizzaInventario();
             }else{
-                System.out.println("Comando non valido");
+                System.out.println(">> Comando non valido");
             }
 
         }
@@ -52,7 +73,7 @@ public class EngineGioco {
             giocatore.setStanzaAttuale(stanza); // se esiste una stanza in quella direzione, allora ci spostiamo
         }
         else{
-            System.out.println("Non puoi andare a " + direzione);
+            System.out.println(">> Non puoi andare a " + direzione);
         }
     }
 
@@ -63,19 +84,26 @@ public class EngineGioco {
 //                .findFirst().orElse(null);
 
         List<Oggetto> oggettiPerTerra = giocatore.getStanzaAttuale().getOggettiPresenti();
-        Oggetto oggetto = oggettiPerTerra.get(oggettiPerTerra.indexOf(nomeOggetto));
+        Oggetto oggettoTrovato = null;
 
-        if(oggetto != null){ // se l'oggetto esiste
-            giocatore.getInventario().aggiungiOggetto(oggetto); // lo aggiungiamo all'inventario
-            giocatore.getStanzaAttuale().getOggettiPresenti().remove(oggetto); // e lo rimuoviamo dalla stanza
+        for(Oggetto oggettoPerTerra : oggettiPerTerra){
+            if(oggettoPerTerra.getNome().equalsIgnoreCase(nomeOggetto)){
+                oggettoTrovato = oggettoPerTerra;
+                break;
+            }
+        }
+
+        if(oggettoTrovato != null){ // se l'oggetto esiste
+            giocatore.getInventario().aggiungiOggetto(oggettoTrovato); // lo aggiungiamo all'inventario
+            giocatore.getStanzaAttuale().getOggettiPresenti().remove(oggettoTrovato); // e lo rimuoviamo dalla stanza
 
             // test per rimovere l'oggetto a terra dalla descrizione della stanza
-            giocatore.getStanzaAttuale().setDescrizione(rimuoviOggettoDaTerra(giocatore.getStanzaAttuale().getDescrizione(), oggetto.getNome())); // da testare
+            giocatore.getStanzaAttuale().setDescrizione(rimuoviOggettoDaTerra(giocatore.getStanzaAttuale().getDescrizione(), oggettoTrovato.getNome())); // da testare
 
-            System.out.println("Hai raccolto: " + oggetto.getNome());
+            System.out.println(">> Hai raccolto: " + oggettoTrovato.getNome());
         }
         else{
-            System.out.println("Oggetto non trovato");
+            System.out.println(">> Oggetto non trovato");
         }
     }
     private void guarda(){
@@ -83,7 +111,7 @@ public class EngineGioco {
 
         List<Oggetto> oggetti = giocatore.getStanzaAttuale().getOggettiPresenti();
         if(oggetti != null && !oggetti.isEmpty()){
-            System.out.println("A terra:");
+            System.out.println(">> A terra:");
             for(Oggetto oggetto : oggetti){
                 System.out.println("- " + oggetto.getNome() + ": " + oggetto.getDescrizione());
             }
@@ -93,20 +121,12 @@ public class EngineGioco {
         StringBuilder risultato = new StringBuilder();
 
         // regex per dividere la descrizioni in blocchi in base ai punti
-        String[] blocchi = descrizioneStanza.split("(?<=\\.)\\s*");
+        String[] frasi = descrizioneStanza.split("(?<=\\.)\\s*");
 
-        for (String blocco : blocchi) {
-            // trovo gli indici di inizio e di fine, basandomi sui punti
-            int inizio = blocco.indexOf('.');
-            int fine = blocco.indexOf('.', inizio + 1);
-
-            if (inizio != -1 && fine != -1) {
-                String contenuto = blocco.substring(inizio + 1, fine).trim();
-
-                // se NON contiene la parola da rimuovere lo aggiungiamo al risultato
-                if (!contenuto.toLowerCase().contains(nomeOggetto.toLowerCase())) {
-                    risultato.append(blocco.substring(0, fine + 1));
-                }
+        for (String frase : frasi) {
+            // se la frase non ha il nome dell'oggetto nel nome, allora la manteniamo
+            if(!frase.toLowerCase().contains(nomeOggetto.toLowerCase())){
+                risultato.append(frase).append(" ");
             }
         }
 
