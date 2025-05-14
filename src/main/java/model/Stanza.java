172015aa2137f1369a7e2c57421046270848d616
vinkstandard <merdacain@hotmail.it>
2025-05-14 11:35:26 +0200

    Aggiunto constructor, e metodi vari

--- src/main/java/model/Stanza.java
+++ src/main/java/model/Stanza.java
@@ -7,5 +7,43 @@ public class Stanza {
     private Map<String, Stanza> uscite;
     private List<Oggetto> oggettiPresenti;
 
+    public Stanza(String nome, String descrizione, Map<String, Stanza> uscite, List<Oggetto> oggettiPresenti) {
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.uscite = uscite;
+        this.oggettiPresenti = oggettiPresenti;
+    }
+
+    public void aggiungiUscita(String direzioneUscita, Stanza stanza){
+        uscite.put(direzioneUscita, stanza);
+    }
+    public Stanza getUscita(String direzioneUscita){
+        return uscite.get(direzioneUscita);
+    }
+    public void aggiungiOggetto(Oggetto oggetto){
+        oggettiPresenti.add(oggetto);
+    }
+
+    public List<Oggetto> getOggettiPresenti(){
+        return oggettiPresenti;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+    public void stampaDescrizioneStanza(){
+        System.out.println(descrizione);
+    }
+
+    public void stampaUscite(){
+        System.out.println("Uscite disponibili:");
+        for(String direzione : uscite.keySet()){
+            System.out.println("- " + direzione);
+        }
+    }
     // TODO: parametro nemiciStanza, eventiAutomatici, condizioniAccesso
 }
