c022e939880f5e3ac6553a9a9a396fa5104ffa60
vinkstandard <merdacain@hotmail.it>
2025-05-15 15:08:07 +0200

    Aggiunti campi "usciteSbloccate", "condizioniSblocco" e metodi

--- src/main/java/model/Stanza.java
+++ src/main/java/model/Stanza.java
@@ -5,15 +5,9 @@ public class Stanza {
     private String nome;
     private String descrizione;
     private Map<String, Stanza> uscite;
+    private Map<String, Boolean> usciteSbloccate; // direzione + sbloccata o no
+    private Map<String, String> condizioniSblocco;
     private List<Oggetto> oggettiPresenti;
-
-    // costruttore completo
-    public Stanza(String nome, String descrizione, Map<String, Stanza> uscite, List<Oggetto> oggettiPresenti) {
-        this.nome = nome;
-        this.descrizione = descrizione;
-        this.uscite = uscite;
-        this.oggettiPresenti = oggettiPresenti;
-    }
 
     // costruttore semplificato (per semplificare la creazione di stanze nel main)
     public Stanza(String nome, String descrizione) {
@@ -21,13 +15,33 @@ public class Stanza {
         this.descrizione = descrizione;
         this.uscite = new HashMap<>();
         this.oggettiPresenti = new ArrayList<>();
+        this.usciteSbloccate = new HashMap<>();
+        this.condizioniSblocco = new HashMap<>();
     }
 
-    public void aggiungiUscita(String direzioneUscita, Stanza stanza){
-        uscite.put(direzioneUscita, stanza);
+    public void aggiungiUscita(String direzione, Stanza stanzaDestinazione, boolean bloccata, String oggettoPerSbloccare) {
+        uscite.put(direzione, stanzaDestinazione);
+        usciteSbloccate.put(direzione, !bloccata);
+        if (bloccata && oggettoPerSbloccare != null) {
+            condizioniSblocco.put(direzione, oggettoPerSbloccare.toLowerCase());
+        }
     }
     public Stanza getUscita(String direzioneUscita){
-        return uscite.get(direzioneUscita);
+        if(usciteSbloccate.getOrDefault(direzioneUscita,true)){
+            return uscite.get(direzioneUscita);
+        }else{
+            return null; // se l'uscita è presente, ma ancora bloccata
+        }
+    }
+    public boolean sbloccaUscita(String direzione, String nomeOggetto) {
+        if (condizioniSblocco.containsKey(direzione) && condizioniSblocco.get(direzione).equalsIgnoreCase(nomeOggetto)) {
+            usciteSbloccate.put(direzione, true);
+            return true;
+        }
+        return false;
+    }
+    public boolean esisteUscita(String direzione){
+        return uscite.containsKey(direzione);
     }
     public void aggiungiOggetto(Oggetto oggetto){
         oggettiPresenti.add(oggetto);
@@ -58,5 +72,19 @@ public class Stanza {
             System.out.println("- " + direzione);
         }
     }
+
+    public Map<String, Stanza> getUscite() {
+        return uscite;
+    }
+
+    public Map<String, Boolean> getUsciteSbloccate() {
+        return usciteSbloccate;
+    }
+
+    public Map<String, String> getCondizioniSblocco() {
+        return condizioniSblocco;
+    }
+
+
     // TODO: parametro nemiciStanza, eventiAutomatici, condizioniAccesso
 }
