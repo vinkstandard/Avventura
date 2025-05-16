676b9c2023412030cd3a997f7e3b2acaea3917ea
vinkstandard <merdacain@hotmail.it>
2025-05-16 15:34:29 +0200

    reformat

--- src/main/java/gioco/Mappa.java
+++ src/main/java/gioco/Mappa.java
@@ -6,6 +6,7 @@ import model.Oggetto;
 
 import java.util.HashMap;
 import java.util.Map;
+
 public class Mappa {
     private Map<String, Stanza> stanze;
     private Stanza stanzaIniziale;
@@ -14,6 +15,7 @@ public class Mappa {
         this.stanze = new HashMap<>();
         costruisciMappa();
     }
+
     private void costruisciMappa() {
 
         // queste sono tutte le mappe, per ora le scrivo qui, ma ho letto che si possono importare tramite un .xml
@@ -23,10 +25,10 @@ public class Mappa {
         Stanza cancelloArrugginito = new Stanza("Cancello arrugginito", "Sei davanti a un cancello arrugginito, " +
                 "oltre in cancello riesci a scrutare una strada sterrata che conduce ad un grande edificio. Ai tuoi piedi c'è una spranga di metallo.");
 
-        Stanza stradaSterrata = new Stanza("Strada sterrata" , "Sei su una strada sterrata, a nord la strada conduce ad una villa gotica, " +
+        Stanza stradaSterrata = new Stanza("Strada sterrata", "Sei su una strada sterrata, a nord la strada conduce ad una villa gotica, " +
                 "verso est noti delle orme nel fango conducono ad un vecchio capanno");
 
-        Stanza capannoAttrezzi = new Stanza("Capanno per gli attrezzi" , "Sei davanti ad un vecchio capanno per gli attrezzi in disuso." +
+        Stanza capannoAttrezzi = new Stanza("Capanno per gli attrezzi", "Sei davanti ad un vecchio capanno per gli attrezzi in disuso." +
                 " La porta del capanno è a terra fradicia. Riesci a scrutare una vecchia lanterna a olio sul bancone.");
 
         Stanza fronteVilla = new Stanza("Ingresso nord-Villa", "Sei davanti all'ingresso principale della villa, la porta è in legno robusto e non ha una serratura. " +
@@ -46,30 +48,29 @@ public class Mappa {
 
 
 //         sterpaglie, questa sarà la stanza iniziale, la cambierò di tanto in tanto per debuggare, facendo spawnare il player dove voglio
-        sterpaglie.aggiungiUscita("nord", cancelloArrugginito,false,null);
+        sterpaglie.aggiungiUscita("nord", cancelloArrugginito, false, null);
 
 //        cancello arrugginito
-        cancelloArrugginito.aggiungiUscita("sud" , sterpaglie, false, null);
-        cancelloArrugginito.aggiungiUscita("nord" , stradaSterrata, true, "spranga");
+        cancelloArrugginito.aggiungiUscita("sud", sterpaglie, false, null);
+        cancelloArrugginito.aggiungiUscita("nord", stradaSterrata, true, "spranga");
         Oggetto spranga = new Oggetto("Spranga arrugginita", "Una Spranga di metallo arrugginita", true, true, null);
         cancelloArrugginito.aggiungiOggetto(spranga);
 
 //        strada sterrata
-        stradaSterrata.aggiungiUscita("est" , capannoAttrezzi,false,null);
-        stradaSterrata.aggiungiUscita("sud", cancelloArrugginito,false,null);
-        stradaSterrata.aggiungiUscita("nord", fronteVilla, false,null);
-        // da fare == stradaSterrata.aggiungiUscita("nord" , ingressoVilla);
+        stradaSterrata.aggiungiUscita("est", capannoAttrezzi, false, null);
+        stradaSterrata.aggiungiUscita("sud", cancelloArrugginito, false, null);
+        stradaSterrata.aggiungiUscita("nord", fronteVilla, false, null);
 
 //        capanno attrezzi
-        capannoAttrezzi.aggiungiUscita("ovest" , stradaSterrata,false,null);
+        capannoAttrezzi.aggiungiUscita("ovest", stradaSterrata, false, null);
         Oggetto lanterna = new Oggetto("Lanterna", "Vecchia lanterna a olio, scuotendola senti che il serbatoio è pieno a metà.", true, true, null);
         capannoAttrezzi.aggiungiOggetto(lanterna);
 
 //        fronte villa
         Oggetto sfera = new Oggetto("Sfera", "Una sfera in vetro decorato.", true, true, null);
         fronteVilla.aggiungiUscita("sud", stradaSterrata, false, null);
-        fronteVilla.aggiungiUscita("ovest" , ovestVilla, false, null);
-        fronteVilla.aggiungiUscita("nord" , atrioVilla, true, "sfera");
+        fronteVilla.aggiungiUscita("ovest", ovestVilla, false, null);
+        fronteVilla.aggiungiUscita("nord", atrioVilla, true, "sfera");
 
 
 //        ovest villa
@@ -80,8 +81,8 @@ public class Mappa {
         Oggetto coltello = new Oggetto("Coltello", "Un coltello da cucina ben affilato", true, true, null);
         cucinaVilla.aggiungiOggetto(coltello);
         cucinaVilla.aggiungiUscita("sud", ovestVilla, false, null);
-        cucinaVilla.aggiungiUscita("est", atrioVilla, false,null);
-        cucinaVilla.aggiungiUscita("nord" , salaDaPranzoVilla, false,null);
+        cucinaVilla.aggiungiUscita("est", atrioVilla, false, null);
+        cucinaVilla.aggiungiUscita("nord", salaDaPranzoVilla, false, null);
 
 //        sala da pranzo
         salaDaPranzoVilla.aggiungiUscita("sud", cucinaVilla, false, null);
