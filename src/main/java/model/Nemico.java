f108aa12550b20bf309411b48b2637bdf86ce2f7
vinkstandard <merdacain@hotmail.it>
2025-05-17 12:35:31 +0200

    Aggiunto calcolo della critchance nel metodo, aggiunto metodo "getDanno()"

--- src/main/java/model/Nemico.java
+++ src/main/java/model/Nemico.java
@@ -1,14 +1,18 @@
 package model;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Nemico {
     private String nome;
     private int vita;
     private int danno;
+    private int possibilitaCritico;
 
-    public Nemico(String nome, int vita, int danno) {
+    public Nemico(String nome, int vita, int danno, int possibilitaCritico) {
         this.nome = nome;
         this.vita = vita;
         this.danno = danno;
+        this.possibilitaCritico = possibilitaCritico;
     }
 
     public String getNome() {
@@ -23,9 +27,18 @@ public class Nemico {
         if(this.vita < 0) this.vita = 0;
     }
     public int attacca(){
-        return danno;
+        int random = ThreadLocalRandom.current().nextInt(0, 101); // calcolo percentuale
+        if(random <= possibilitaCritico){ // se abbiamo crittato
+            return danno * 2;
+        }else{
+            return danno;
+        }
     }
     public boolean isVivo(){
         return vita > 0;
+    }
+
+    public int getDanno() {
+        return danno;
     }
 }
