1f0bff0e6904a58dd07860c93fbc3324525a56c4
vinkstandard <merdacain@hotmail.it>
2025-05-17 12:33:23 +0200

    Aggiunta classe arma

---
+++ src/main/java/model/Arma.java
@@ -0,0 +1,31 @@
+package model;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Arma extends Oggetto {
+
+    private int danno;
+    private int possibilitaCritico;
+
+    public Arma(String nome, String descrizione, int danno, int possibilitaCritico){
+        super(nome, descrizione);
+        this.danno = danno;
+        this.possibilitaCritico = possibilitaCritico;
+    }
+    public int infliggiDanno(){
+        int random = ThreadLocalRandom.current().nextInt(0, 101); // calcolo percentuale
+        if(random <= possibilitaCritico){ // se abbiamo crittato
+            return danno * 2;
+        }else{
+            return danno;
+        }
+    }
+
+    public int getDanno() {
+        return danno;
+    }
+
+    public int getPossibilitaCritico() {
+        return possibilitaCritico;
+    }
+}
