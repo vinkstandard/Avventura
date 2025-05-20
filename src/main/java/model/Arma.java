510d0fb531f4d665a413d8ebbc87cf25553c0bf0
vinkstandard <merdacain@hotmail.it>
2025-05-20 14:43:36 +0200

    refact

--- src/main/java/model/Arma.java
+++ src/main/java/model/Arma.java
@@ -7,16 +7,19 @@ public class Arma extends Oggetto {
     private int danno;
     private int possibilitaCritico;
 
-    public Arma(String nome, String descrizione, int danno, int possibilitaCritico){
+    public Arma(String nome, String descrizione, int danno, int possibilitaCritico) {
+
         super(nome, descrizione, true); // raccoglibile di default
         this.danno = danno;
         this.possibilitaCritico = possibilitaCritico;
     }
-    public int infliggiDanno(){
+
+    public int infliggiDanno() {
+
         int random = ThreadLocalRandom.current().nextInt(0, 101); // calcolo percentuale
-        if(random <= possibilitaCritico){ // se abbiamo crittato
+        if (random <= possibilitaCritico) { // se abbiamo crittato
             return danno * 2;
-        }else{
+        } else {
             return danno;
         }
     }
